1decab23c0eb7f7117c3e2607e2e83dec2bffcc1
broski-ag <broskiagency@gmail.com>
2025-07-30 10:32:50 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -526,11 +526,10 @@ ScrollTrigger.create({
         </div>
 
         {/* Floating Testimonial Badges */}
-        
+         
                  <div className="fixed inset-0 z-20 pointer-events-none">
           {testimonialBadges.map((badge, index) => (
             <TestimonialBadge key={index} badge={badge} />
-     {showTestimonials && (
           ))}
         </div>
 
