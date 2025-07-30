5309f767cac995df7769d050e31e7c759de2b9e2
broski-ag <broskiagency@gmail.com>
2025-07-30 11:29:46 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -259,8 +259,8 @@ function App() {
   // Mouse tracking effect
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
-      const x = (e.clientX / window.innerWidth - 0.2) * 0.9; // -1 to 1
-      const y = (e.clientY / window.innerHeight - 0.2) * 0.9; // -1 to 1
+      const x = (e.clientX / window.innerWidth - 0.2) * 2; // -1 to 1
+      const y = (e.clientY / window.innerHeight - 0.2) * 0.7; // -1 to 1
       setMousePosition({ x, y });
     };
 
